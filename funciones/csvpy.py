2b693adaf88c5395301f85afb17c805c9be4507d
Odin2 <alepiojo05@gmail.com>
2018-03-04 21:55:38 -0600

    finalizar funcion login + funcion cargar csv

---
+++ funciones/csvpy.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import csv
+
+
+
+class datoscsv:
+
+    def __init__(self,id1,boneage,male):
+        self.id1 = id1
+        self.boneage = boneage
+        self.male = male
+
+def train():
+    lista = []
+    with open('train.csv') as csvarchivo:  # Abrir archivo csv
+        entrada = csv.reader(csvarchivo)  # Leer todos los registros
+        for reg in entrada:  # Leer registro (lista)
+
+        # print(reg)  # Mostrar registro
+
+        # Leer campos
+
+            m = datoscsv(reg[0], reg[1], reg[2])
+            lista.append(m)
+            print(m.id1 + ' ' + m.boneage + ' ' + m.male)  # mostrar objeto
+
+    csvarchivo.close()
+
+
+
+            
