2343e5d555451d17dfec4c77e53c8440ed614409
Odin2 <alepiojo05@gmail.com>
2018-05-03 00:25:29 -0600

    Formatting

--- funciones/csvpy.py
+++ funciones/csvpy.py
@@ -3,33 +3,31 @@
 import csv
 
 
-
 class datoscsv:
 
-    def __init__(self,id1,boneage,male):
+    def __init__(self, id1, boneage, male):
         self.id1 = id1
         self.boneage = boneage
         self.male = male
-        
-## Ejemplo de lectura y manipulacion de datos en un csv.
+
+# Ejemplo de lectura y manipulacion de datos en un csv.
 #
 #  Imprime todos los datos a la consola de python.
+
+
 def train():
     lista = []
     with open('train.csv') as csvarchivo:  # Abrir archivo csv
         entrada = csv.reader(csvarchivo)  # Leer todos los registros
         for reg in entrada:  # Leer registro (lista)
 
-        # print(reg)  # Mostrar registro
+            # print(reg)
+            # Mostrar registro
 
-        # Leer campos
+            # Leer campos
 
             m = datoscsv(reg[0], reg[1], reg[2])
             lista.append(m)
             print(m.id1 + ' ' + m.boneage + ' ' + m.male)  # mostrar objeto
 
     csvarchivo.close()
-
-
-
-            
